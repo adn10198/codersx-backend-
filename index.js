3ede77f4e373192cc49f9da5b8b94ff9ab5c2b76
ADN <anhdungngo1001@gmail.com>
2020-09-11 22:51:44 +0700

    sssdsd

--- index.js
+++ index.js
@@ -2,14 +2,14 @@ var express = require('express');
 var app = express();
 
 var userRoute = require('./routes/user.route')
+var userController = require('./controllers/user.controller')
+
 
 var port = 3000;
 
 app.listen(port, function () {});
 
-app.get('/', function (req, res) {
-	res.send('helooo');
-});
+app.get('/', userController.index);
 app.get('/user', function (req, res) {
 	res.send('hello usser');
 });
